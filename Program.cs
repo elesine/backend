a1cd69e4840bedf03c311936788733a21fb1aaf4
Gretel Danitza Canasa Condori <gcanasac@bsginstitute.com>
2022-12-13 19:10:06 -0500

    middleware-log

--- Program.cs
+++ Program.cs
@@ -18,6 +18,37 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.Use(async (context, next) =>
+{
+    using (var swapStream =  new MemoryStream())
+    {
+        var respuestaOriginal = context.Response.Body;
+        context.Response.Body = swapStream;
+
+        await next.Invoke();
+
+        swapStream.Seek(0, SeekOrigin.Begin);
+        string respuesta = new StreamReader(swapStream).ReadToEnd();
+        swapStream.Seek(0,SeekOrigin.Begin);
+
+        await swapStream.CopyToAsync(respuestaOriginal);
+        context.Response.Body = respuestaOriginal;
+
+        app.Logger.LogInformation(respuesta);
+
+    }
+});
+
+app.Map("/mapa1", (app) =>
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("Estoy interceptando el pipeline");
+    });
+});
+
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
